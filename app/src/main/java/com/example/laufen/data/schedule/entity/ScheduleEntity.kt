553c7b93db8f6734893756ddd9a4ab4c877324e2
capkeik <new.mail2018@mail.ru>
2022-07-11 22:25:14 +0300

    fix: equals bug. override equals() for ScheduleEntity

--- app/src/main/java/com/example/laufen/data/schedule/entity/ScheduleEntity.kt
+++ app/src/main/java/com/example/laufen/data/schedule/entity/ScheduleEntity.kt
@@ -5,4 +5,11 @@ data class ScheduleEntity(
     val minute: Int,
     val dayOfWeek: Int,
     var millis: Long = 0
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        return other is ScheduleEntity &&
+                other.hour == hour &&
+                other.dayOfWeek == dayOfWeek &&
+                other.minute == minute
+    }
+}
